1cb3f747abca6c7888d704e443b6f0df5da387db
Rico Hermsen <rico@jrny.nl>
2026-02-18 17:18:41 +0100

    feat: next.js

---
+++ web/next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+    // Github pages setup
+    output: "export",
+    trailingSlash: true,
+    // images: { unoptimized: true },
+};
+
+export default nextConfig;
