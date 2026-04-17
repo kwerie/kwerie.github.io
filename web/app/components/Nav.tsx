f07fd6209d42a16c934ff8f95c5b47bad64d70b6
Rico Hermsen <rico@jrny.nl>
2026-04-17 18:00:56 +0200

    chore: dark mode toggle improvements and nav changes

--- web/app/components/Nav.tsx
+++ web/app/components/Nav.tsx
@@ -1,15 +1,15 @@
 export default async function Nav() {
     return (
-        <div className="container flex items-center">
-            <div className="text-2xl font-bold before:content-['~/'] shell-prefix">kwerie</div>
-            <div className="ml-auto flex items-center gap-4">
-                <a href="#work" className="text-md">
+        <div className="container flex items-center py-4 border-b dark:border-zinc-700">
+            <div className="text-3xl dark:text-zinc-50 font-bold before:content-['~/'] shell-prefix select-none">kwerie</div>
+            <div className="ml-auto flex items-center gap-4 text-2xl">
+                <a href="#work">
                     Work
                 </a>
-                <a href="#projects" className="text-md">
+                <a href="#projects">
                     Projects
                 </a>
-                <a href="#contact" className="text-md">
+                <a href="#contact">
                     Contact
                 </a>
             </div>
--- web/app/components/ThemeToggle.tsx
+++ web/app/components/ThemeToggle.tsx
@@ -5,7 +5,9 @@ import { useTheme } from "../providers/ThemeProvider";
 
 export function ThemeToggle(): React.ReactNode {
     const { theme, toggleTheme } = useTheme();
-    return (<div className="absolute right-[5] bottom-[5]">
-        <div className="before:content-['{'] after:content-['}'] shell-prefix select-none cursor-pointer" onClick={() => toggleTheme()}>{theme}</div>
-    </div>);
+    return (
+        <div className="absolute right-[10] bottom-[10] border border-primary dark:border-zinc-50 p-2">
+            <div className="before:content-['{'] after:content-['}'] shell-prefix select-none cursor-pointer" onClick={toggleTheme}>{theme}</div>
+        </div>
+    );
 }
